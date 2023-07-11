fb227e91ae722a678a4d758459e4fa5ce6c70dd7
junseo <cloneot@gmail.com>
2023-07-11 22:05:43 +0900

    feat(join): add join feat

--- lib/resources/socket_methods.dart
+++ lib/resources/socket_methods.dart
@@ -8,7 +8,6 @@ import 'package:socket_io_client/socket_io_client.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 import '../provider/room_list_provider.dart';
-import '../screens/join_room_screen.dart';
 
 class SocketMethods {
   final _socketClient = SocketClient.instance.socket!;
@@ -25,6 +24,7 @@ class SocketMethods {
   //방 참여 emit
   void joinRoom(String nickName, String roomId) {
     if (nickName.isNotEmpty && roomId.isNotEmpty) {
+      print('joinRoom emit: nickName: $nickName, roomId: $roomId');
       _socketClient.emit('joinRoom', {
         'nickName': nickName,
         'roomId': roomId,
@@ -197,7 +197,6 @@ class SocketMethods {
       print('getRoomListListener success: $roomList');
       Provider.of<RoomListProvider>(context, listen: false)
           .updateRoomList(roomList);
-      Navigator.pushNamed(context, JoinRoomScreen.routeName);
     });
   }
 }
